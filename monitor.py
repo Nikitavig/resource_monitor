73aa1b70853393c1509ce1874aaf29e01fbf82d9
Nikitavig <nikitavig@mail.ru>
2020-03-09 22:32:00 +0300

    comments

--- monitor.py
+++ monitor.py
@@ -2,8 +2,10 @@ import time
 import os
 import psutil
 
+
 # Функция очистки жкрана
 CLEAR = lambda: os.system('cls')
+
 
 def main():
 	"""
@@ -17,12 +19,19 @@ def main():
 	while True:
 		# Вызываем lmbda функцию для очистки экрана
 		CLEAR()
+		# Выводим загрузку процессора в %
 		print(f"CPU     : {psutil.cpu_percent(interval=None)} %")
+		# Выводим используемую память в %, потом в МБ
 		print(f"Memory  : {psutil.virtual_memory()[2]} % | {round(psutil.virtual_memory()[3] / 1024 / 1024, 1)} MB")
+		# Выводим используемую swap память в %, потом в МБ
 		print(f"Swap M  : {psutil.swap_memory()[3]} % | {round(psutil.swap_memory()[1] / 1024 / 1024, 1)} MB")
+		# Использование сети
 		print(f"********** NETWORK **********")
+		# Выводим принятиый по сети трафик
 		print(f"received: {round(psutil.net_io_counters(pernic=False, nowrap=True)[1] / 1024 / 1024, 1)} MB")
+		# Выводим полученный по сети
 		print(f"send    : {round(psutil.net_io_counters(pernic=False, nowrap=True)[0] / 1024 / 1024, 1)} MB")
+		# Пауза
 		time.sleep(0.5)
 
 
